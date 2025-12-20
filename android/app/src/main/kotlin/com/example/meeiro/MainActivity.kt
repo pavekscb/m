7c71d7601a48ff32d942835fbaa2dcda9d7c713e
Pavel <70694092+pavekscb@users.noreply.github.com>
2025-12-20 20:28:40 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/meeiro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meeiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/build.gradle.kts
@@ -0,0 +1,40 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // ПРИНУДИТЕЛЬНЫЙ ОТКАТ (самое важное)
+    configurations.all {
+        resolutionStrategy {
+            force("androidx.browser:browser:1.8.0")
+            force("androidx.core:core:1.13.1")
+            force("androidx.core:core-ktx:1.13.1")
+            force("androidx.lifecycle:lifecycle-common:2.7.0")
+            force("androidx.lifecycle:lifecycle-runtime:2.7.0")
+            force("androidx.lifecycle:lifecycle-process:2.7.0")
+        }
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
